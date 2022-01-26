f4b415bd48a3acc31199d3ad54088e885b1cb2c1
luirusso <luirusso_@hotmail.it>
2022-01-26 18:35:02 +0100

    added data to comic controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Comic;
 
 class ComicController extends Controller
@@ -37,7 +38,26 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        return 'SAVE COMIC TO DATABASE';
+        $data = $request->all();
+
+        // VALIDATION
+
+        // INJECT INTO DATABASE
+        $new_comic = new Comic();
+
+        $new_comic->title = $data['title'];
+        $new_comic->slug = Str::slug($data['title'], '-');
+        $new_comic->description = $data['description'];
+        $new_comic->thumb = $data['thumb'];
+        $new_comic->price = $data['price'];
+        $new_comic->series = $data['series'];
+        $new_comic->sale_date = $data['sale_date'];
+        $new_comic->type = $data['type'];
+
+        $new_comic->save();
+
+        // REDIRECT TO NEW SHOW DETAIL PAGE
+        return redirect()->route('comics.show', $new_comic->id);
     }
 
     /**
