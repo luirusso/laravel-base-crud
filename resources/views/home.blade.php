0171d350c83f371cda44e9a91a7a810a1cd8ab10
luirusso <luirusso_@hotmail.it>
2022-01-26 15:28:16 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+    <section class="container">
+        <h1>
+            Welcome to Comics Shop
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et exercitationem eveniet rerum, ratione ipsa at cupiditate ipsam in vitae omnis culpa dolor nobis temporibus! Perferendis saepe eos pariatur mollitia provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam harum veritatis dolor, officia aliquam dolorem amet eum quas consectetur magnam libero cum dolore ducimus pariatur eos nesciunt, porro id accusamus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit totam neque impedit exercitationem aut officia enim magnam quas in quisquam eum sed libero, quod ut cum assumenda, laboriosam nihil fugiat?
+        </p>
+    </section>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center mt-5">
+    Comics Shop - All rights reserved.
+</footer>
